3c5d037ead97c9b084f684dc13f63075955bb1a5
virgil2019 <danten0402@gmail.com>
2022-09-08 23:28:39 +0800

    Omniverse NFT

--- contracts/App.sol
+++ contracts/App.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./OmniverseNFT.sol";
 import "@hthuang/contracts/ContractBase.sol";
+import "./OmniverseNFT.sol";
 
-contract OmniverseNFT is ContractBase {
+contract App is ContractBase {
     struct NFTInfo {
-        string uuid;
+        uint64 uuid;
         string domain;
         uint64 id;
         string tokenURL;
@@ -15,31 +15,109 @@ contract OmniverseNFT is ContractBase {
         bytes32 hashValue;
     }
 
+    struct FlowContract {
+        bytes contractAddress;
+        bytes action;
+    }
+
+    // NFT contract
     OmniverseNFT public nftContract;
+    // NFT list that are waiting for being claimed
     NFTInfo[] public pendingNFTList;
+    // Destination chain contract information
+    FlowContract public flowContract;
 
     constructor(address _address) {
         nftContract = OmniverseNFT(_address);
     }
 
+    function setFlowContract(bytes calldata _contract, bytes calldata _action) external onlyOwner {
+        flowContract.contractAddress = _contract;
+        flowContract.action = _action;
+    }
+
     /**
      * @dev Mints a token to a user, it is cross-chain invoked.
      */
-    function ccMint(Payload calldata _payload) public onlyCC returns (uint256) {
+    function ccrMint(Payload calldata _payload) public returns (uint256) {
         if (msg.sender != address(crossChainContract)) {
-            return CALLER_NOT_CROSS_CHAIN_CONTRACT;
+            return 100;
         }
 
-        SimplifiedMessage memory context = getContext();
-
         NFTInfo storage nftInfo = pendingNFTList.push();
-        (nftInfo.uuid) = abi.decode(_payload.items[0].value, (string));
-        (nftInfo.domain) = abi.decode(_payload.items[0].value, (string));
-        (nftInfo.id) = abi.decode(_payload.items[0].value, (uint64));
-        (nftInfo.tokenURL) = abi.decode(_payload.items[0].value, (string));
-        (nftInfo.receiver) = abi.decode(_payload.items[0].value, (address));
-        (nftInfo.hashValue) = abi.decode(_payload.items[0].value, (bytes32));
+        (nftInfo.uuid) = abi.decode(_payload.items[0].value, (uint64));
+        (nftInfo.domain) = abi.decode(_payload.items[1].value, (string));
+        (nftInfo.id) = abi.decode(_payload.items[2].value, (uint64));
+        (nftInfo.tokenURL) = abi.decode(_payload.items[3].value, (string));
+        (bytes memory bReceiver) = abi.decode(_payload.items[4].value, (bytes));
+        address receiver;
+        assembly {
+            receiver := mload(add(bReceiver, 20))
+        }
+        nftInfo.receiver = receiver;
+        (bytes memory bHash) = abi.decode(_payload.items[5].value, (bytes));
+        bytes32 hashValue;
+        assembly {
+            hashValue := mload(add(bHash, 32))
+        }
+        nftInfo.hashValue = hashValue;
 
         return 0;
+    }
+
+    /**
+     * @dev Claims a token in the pending list
+     */
+    function claimToken(uint256 tokenId, string calldata answer) external {
+        bool found = false;
+        uint256 index = 0;
+        for (uint256 i = 0; i < pendingNFTList.length; i++) {
+            index = i;
+            if (pendingNFTList[i].receiver == msg.sender) {
+                found = true;
+                break;
+            }
+        }
+        require(found, "Token not exists");
+
+        bytes32 h = keccak256(abi.encodePacked(answer));
+        require(h == pendingNFTList[index].hashValue, "Wrong answer");
+        require(msg.sender == pendingNFTList[index].receiver, "Caller not the receiver");
+
+        nftContract.mint(msg.sender, tokenId, pendingNFTList[index].tokenURL);
+    }
+
+    /**
+     * @dev Burns a token of a user, then a cross-chain call will be called to
+     *      unlock a token on Flow.
+     */
+    function ccsUnlock(uint256 tokenId, bytes calldata receiver, bytes32 hashValue) external {
+        address owner = nftContract.ownerOf(tokenId);
+        require(owner == msg.sender, "Caller not owner");
+
+        nftContract.burn(tokenId);
+
+        // Construct payload
+        Payload memory data;
+        data.items = new PayloadItem[](3);
+        PayloadItem memory item0 = data.items[0];
+        item0.name = "uuid";
+        item0.msgType = MsgType.EvmU64;
+        item0.value = abi.encode(uint64(tokenId));
+        PayloadItem memory item1 = data.items[1];
+        item1.name = "receiver";
+        item1.msgType = MsgType.Bytes;
+        item1.value = receiver;
+        PayloadItem memory item2 = data.items[2];
+        item2.name = "hashValue";
+        item2.msgType = MsgType.Bytes;
+        item2.value = abi.encode(hashValue);
+
+        ISentMessage memory message;
+        message.toChain = "FLOW";
+        message.session = Session(0, 0, "", "", "");
+        message.content = Content(flowContract.contractAddress, flowContract.action, data);
+
+        crossChainContract.sendMessage(message);
     }
 }
--- contracts/OmniverseNFT.sol
+++ contracts/OmniverseNFT.sol
@@ -5,12 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OmniverseNFT is ERC721, Ownable {
+    mapping(uint256 => string) private tokenURIs;
+
     constructor() ERC721("OMNIVERSE-NFT", "OMNIVERSE-NFT") {
 
     }
 
-    function mint(address to, uint256 tokenId) external onlyOwner() {
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return tokenURIs[tokenId];
+    }
+
+    function mint(address to, uint256 tokenId, string calldata uri) external onlyOwner() {
         _mint(to, tokenId);
+        tokenURIs[tokenId] = uri;
     }
 
     function burn(uint256 tokenId) external onlyOwner() {
